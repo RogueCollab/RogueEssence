abe209510d635fc5b8f71590632bb7b5e8abcfe9
audino <2676737+audinowho@users.noreply.github.com>
2021-05-15 16:07:08 -0800

    Refactor PoolTeamSpawner

--- RogueEssence/LevelGen/Zones/ZonePostProcs/ZoneTeamSpawnPostProc.cs
+++ RogueEssence/LevelGen/Zones/ZonePostProcs/ZoneTeamSpawnPostProc.cs
@@ -16,16 +16,7 @@ namespace RogueEssence.LevelGen
 
         [SubGroup]
         [RangeBorder(0, true, true)]
-        public SpawnRangeList<MobSpawn> NormalSpawns;
-        [SubGroup]
-        [RangeBorder(0, true, true)]
-        public SpawnRangeList<MobSpawn> LonerSpawns;
-        [SubGroup]
-        [RangeBorder(0, true, true)]
-        public SpawnRangeList<MobSpawn> LeaderSpawns;
-        [SubGroup]
-        [RangeBorder(0, true, true)]
-        public SpawnRangeList<MobSpawn> SupportSpawns;
+        public SpawnRangeList<TeamMemberSpawn> Spawns;
         [SubGroup]
         [RangeBorder(0, true, true)]
         public SpawnRangeList<int> TeamSizes;
@@ -38,20 +29,14 @@ namespace RogueEssence.LevelGen
 
         public ZoneTeamSpawnPostProc()
         {
-            NormalSpawns = new SpawnRangeList<MobSpawn>();
-            LonerSpawns = new SpawnRangeList<MobSpawn>();
-            LeaderSpawns = new SpawnRangeList<MobSpawn>();
-            SupportSpawns = new SpawnRangeList<MobSpawn>();
+            Spawns = new SpawnRangeList<TeamMemberSpawn>();
             TeamSizes = new SpawnRangeList<int>();
             SpecificSpawns = new SpawnRangeList<SpecificTeamSpawner>();
         }
 
         protected ZoneTeamSpawnPostProc(ZoneTeamSpawnPostProc other, ulong seed) : this()
         {
-            NormalSpawns = other.NormalSpawns;
-            LonerSpawns = other.LonerSpawns;
-            LeaderSpawns = other.LeaderSpawns;
-            SupportSpawns = other.SupportSpawns;
+            Spawns = other.Spawns;
             TeamSizes = other.TeamSizes;
             SpecificSpawns = other.SpecificSpawns;
             Priority = other.Priority;
@@ -64,12 +49,9 @@ namespace RogueEssence.LevelGen
             MobSpawnStep<BaseMapGenContext> spawnStep = new MobSpawnStep<BaseMapGenContext>();
 
             PoolTeamSpawner spawner = new PoolTeamSpawner();
-            spawner.NormalSpawns = NormalSpawns.GetSpawnList(zoneContext.CurrentID);
-            spawner.LonerSpawns = LonerSpawns.GetSpawnList(zoneContext.CurrentID);
-            spawner.LeaderSpawns = LeaderSpawns.GetSpawnList(zoneContext.CurrentID);
-            spawner.SupportSpawns = SupportSpawns.GetSpawnList(zoneContext.CurrentID);
+            spawner.Spawns = Spawns.GetSpawnList(zoneContext.CurrentID);
             spawner.TeamSizes = TeamSizes.GetSpawnList(zoneContext.CurrentID);
-            spawnStep.Spawns.Add(spawner, spawner.NormalSpawns.SpawnTotal + spawner.LeaderSpawns.SpawnTotal + spawner.LonerSpawns.SpawnTotal);
+            spawnStep.Spawns.Add(spawner, spawner.Spawns.SpawnTotal);
 
             SpawnList<SpecificTeamSpawner> specificSpawner = SpecificSpawns.GetSpawnList(zoneContext.CurrentID);
             for (int ii = 0; ii < specificSpawner.Count; ii++)
@@ -80,12 +62,7 @@ namespace RogueEssence.LevelGen
 
         public override string ToString()
         {
-            int totalCount = 0;
-            totalCount += NormalSpawns.Count;
-            totalCount += LonerSpawns.Count;
-            totalCount += LeaderSpawns.Count;
-            totalCount += SupportSpawns.Count;
-            return string.Format("{0}[{1}] Spec:{2}", this.GetType().Name, totalCount.ToString(), this.SpecificSpawns.Count.ToString());
+            return string.Format("{0}[{1}] Spec:{2}", this.GetType().Name, Spawns.Count.ToString(), this.SpecificSpawns.Count.ToString());
         }
     }
 }
