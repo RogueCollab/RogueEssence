7a84131cf808257e501bdbd885f4a5e22809f5b1
audino <2676737+audinowho@users.noreply.github.com>
2021-08-14 10:46:11 -0800

    SpreadStepRangeZoneStep

--- RogueEssence/LevelGen/Zones/ZoneSteps/SpreadStepZoneStep.cs
+++ RogueEssence/LevelGen/Zones/ZoneSteps/SpreadStepZoneStep.cs
@@ -58,4 +58,61 @@ namespace RogueEssence.LevelGen
             }
         }
     }
+
+    [Serializable]
+    public class SpreadStepRangeZoneStep : ZoneStep
+    {
+        public SpreadPlanBase SpreadPlan;
+        public SpawnRangeList<IGenPriority> Spawns;
+
+        [NonSerialized]
+        public List<IGenPriority> DropItems;
+        //spreads an item through the floors
+        //ensures that the space in floors between occurrences is kept tame
+        public SpreadStepRangeZoneStep()
+        {
+        }
+
+        public SpreadStepRangeZoneStep(SpreadPlanBase plan, SpawnRangeList<IGenPriority> spawns) : this()
+        {
+            SpreadPlan = plan;
+            Spawns = spawns;
+        }
+
+        protected SpreadStepRangeZoneStep(SpreadStepRangeZoneStep other, ulong seed) : this()
+        {
+            Spawns = other.Spawns.CopyState();
+            SpreadPlan = other.SpreadPlan.Instantiate(seed);
+
+            DropItems = new List<IGenPriority>();
+
+            //Other SpredStep classes choose which step to place on which floor on the fly, but this one needs care, due to the potential of CanPick changing state
+            for (int ii = 0; ii < SpreadPlan.DropPoints.Count; ii++)
+            {
+                int floor = SpreadPlan.DropPoints[ii];
+                SpawnList<IGenPriority> spawnList = Spawns.GetSpawnList(floor);
+                if (!spawnList.CanPick)
+                    break;
+
+                ReRandom rand = new ReRandom(seed);
+                IGenPriority genStep = spawnList.Pick(rand);
+                DropItems.Add(genStep);
+            }
+        }
+        public override ZoneStep Instantiate(ulong seed) { return new SpreadStepRangeZoneStep(this, seed); }
+
+
+        public override void Apply(ZoneGenContext zoneContext, IGenContext context, StablePriorityQueue<Priority, IGenStep> queue)
+        {
+            for (int ii = 0; ii < SpreadPlan.DropPoints.Count; ii++)
+            {
+                if (SpreadPlan.DropPoints[ii] != zoneContext.CurrentID)
+                    continue;
+                if (ii >= DropItems.Count)
+                    continue;
+                IGenPriority genStep = DropItems[ii];
+                queue.Enqueue(genStep.Priority, genStep.GetItem());
+            }
+        }
+    }
 }
