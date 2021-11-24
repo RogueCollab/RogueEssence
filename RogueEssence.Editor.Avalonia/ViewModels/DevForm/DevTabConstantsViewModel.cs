cb3a21c9df10c9af777c62bcf3a79b5c9364eff5
audino <2676737+audinowho@users.noreply.github.com>
2021-11-23 23:33:02 -0800

    Properly save edited constants in mods

--- RogueEssence.Editor.Avalonia/ViewModels/DevForm/DevTabConstantsViewModel.cs
+++ RogueEssence.Editor.Avalonia/ViewModels/DevForm/DevTabConstantsViewModel.cs
@@ -18,70 +18,70 @@ namespace RogueEssence.Dev.ViewModels
         {
             OpenList<ActiveEffect>("Universal Event", DataManager.Instance.UniversalEvent, (obj) => {
                 DataManager.Instance.UniversalEvent = obj;
-                DataManager.SaveData(PathMod.ModPath(DataManager.DATA_PATH + "Universal.bin"), obj);
+                DataManager.SaveData(PathMod.HardMod(DataManager.DATA_PATH + "Universal.bin"), obj);
             });
         }
         public void btnEditHeal_Click()
         {
             OpenList<BattleFX>("Heal FX", DataManager.Instance.HealFX, (fx) => {
                 DataManager.Instance.HealFX = fx;
-                DataManager.SaveData(PathMod.ModPath(DataManager.FX_PATH + "Heal.fx"), fx);
+                DataManager.SaveData(PathMod.HardMod(DataManager.FX_PATH + "Heal.fx"), fx);
             });
         }
         public void btnEditRestoreCharge_Click()
         {
             OpenList<BattleFX>("Restore Charge FX", DataManager.Instance.RestoreChargeFX, (fx) => {
                 DataManager.Instance.RestoreChargeFX = fx;
-                DataManager.SaveData(PathMod.ModPath(DataManager.FX_PATH + "RestoreCharge.fx"), fx);
+                DataManager.SaveData(PathMod.HardMod(DataManager.FX_PATH + "RestoreCharge.fx"), fx);
             });
         }
         public void btnEditLoseCharge_Click()
         {
             OpenList<BattleFX>("Lose Charge FX", DataManager.Instance.LoseChargeFX, (fx) => {
                 DataManager.Instance.LoseChargeFX = fx;
-                DataManager.SaveData(PathMod.ModPath(DataManager.FX_PATH + "LoseCharge.fx"), fx);
+                DataManager.SaveData(PathMod.HardMod(DataManager.FX_PATH + "LoseCharge.fx"), fx);
             });
         }
         public void btnEditNoCharge_Click()
         {
             OpenList<EmoteFX>("No Charge FX", DataManager.Instance.NoChargeFX, (fx) => {
                 DataManager.Instance.NoChargeFX = fx;
-                DataManager.SaveData(PathMod.ModPath(DataManager.FX_PATH + "NoCharge.fx"), fx);
+                DataManager.SaveData(PathMod.HardMod(DataManager.FX_PATH + "NoCharge.fx"), fx);
             });
         }
         public void btnEditElement_Click()
         {
             OpenList<BattleFX>("Element FX", DataManager.Instance.ElementFX, (fx) => {
                 DataManager.Instance.ElementFX = fx;
-                DataManager.SaveData(PathMod.ModPath(DataManager.FX_PATH + "Element.fx"), fx);
+                DataManager.SaveData(PathMod.HardMod(DataManager.FX_PATH + "Element.fx"), fx);
             });
         }
         public void btnEditIntrinsic_Click()
         {
             OpenList<BattleFX>("Intrinsic FX", DataManager.Instance.IntrinsicFX, (fx) => {
                 DataManager.Instance.IntrinsicFX = fx;
-                DataManager.SaveData(PathMod.ModPath(DataManager.FX_PATH + "Intrinsic.fx"), fx);
+                DataManager.SaveData(PathMod.HardMod(DataManager.FX_PATH + "Intrinsic.fx"), fx);
             });
         }
         public void btnEditSendHome_Click()
         {
             OpenList<BattleFX>("Send Home FX", DataManager.Instance.SendHomeFX, (fx) => {
                 DataManager.Instance.SendHomeFX = fx;
-                DataManager.SaveData(PathMod.ModPath(DataManager.FX_PATH + "SendHome.fx"), fx);
+                DataManager.SaveData(PathMod.HardMod(DataManager.FX_PATH + "SendHome.fx"), fx);
             });
         }
         public void btnEditItemLost_Click()
         {
             OpenList<BattleFX>("Item Lost FX", DataManager.Instance.ItemLostFX, (fx) => {
                 DataManager.Instance.ItemLostFX = fx;
-                DataManager.SaveData(PathMod.ModPath(DataManager.FX_PATH + "ItemLost.fx"), fx);
+                DataManager.SaveData(PathMod.HardMod(DataManager.FX_PATH + "ItemLost.fx"), fx);
             });
         }
         public void btnEditWarp_Click()
         {
             OpenList<BattleFX>("Warp FX", DataManager.Instance.WarpFX, (fx) => {
                 DataManager.Instance.WarpFX = fx;
-                DataManager.SaveData(PathMod.ModPath(DataManager.FX_PATH + "Warp.fx"), fx);
+                DataManager.SaveData(PathMod.HardMod(DataManager.FX_PATH + "Warp.fx"), fx);
             });
         }
 
@@ -89,7 +89,7 @@ namespace RogueEssence.Dev.ViewModels
         {
             OpenList<BattleFX>("Knockback FX", DataManager.Instance.KnockbackFX, (fx) => {
                 DataManager.Instance.KnockbackFX = fx;
-                DataManager.SaveData(PathMod.ModPath(DataManager.FX_PATH + "Knockback.fx"), fx);
+                DataManager.SaveData(PathMod.HardMod(DataManager.FX_PATH + "Knockback.fx"), fx);
             });
         }
 
@@ -97,14 +97,14 @@ namespace RogueEssence.Dev.ViewModels
         {
             OpenList<BattleFX>("Jump FX", DataManager.Instance.JumpFX, (fx) => {
                 DataManager.Instance.JumpFX = fx;
-                DataManager.SaveData(PathMod.ModPath(DataManager.FX_PATH + "Jump.fx"), fx);
+                DataManager.SaveData(PathMod.HardMod(DataManager.FX_PATH + "Jump.fx"), fx);
             });
         }
 
         public void btnEditThrow_Click()
         {
             OpenList<BattleFX>("Throw FX", DataManager.Instance.ThrowFX, (fx) => { DataManager.Instance.ThrowFX = fx;
-                DataManager.SaveData(PathMod.ModPath(DataManager.FX_PATH + "Throw.fx"), fx);
+                DataManager.SaveData(PathMod.HardMod(DataManager.FX_PATH + "Throw.fx"), fx);
             });
         }
 
