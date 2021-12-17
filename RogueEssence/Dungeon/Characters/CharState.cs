85e0cdfb4500339d3eef5f65a5c06494204661b9
audino <2676737+audinowho@users.noreply.github.com>
2021-12-17 12:21:32 -0800

    ModGen CharStates

--- RogueEssence/Dungeon/Characters/CharState.cs
+++ RogueEssence/Dungeon/Characters/CharState.cs
@@ -5,7 +5,20 @@ namespace RogueEssence.Dungeon
     [Serializable]
     public abstract class CharState : GameplayState
     {
-        
+
     }
+
+
+    [Serializable]
+    public abstract class ModGenState : CharState
+    {
+        public int Mod;
+
+        public ModGenState() { }
+        public ModGenState(int mod) { Mod = mod; }
+
+        protected ModGenState(ModGenState other) { Mod = other.Mod; }
+    }
+
 
 }
