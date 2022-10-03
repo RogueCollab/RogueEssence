b1ac844de321f86307351adac5c73ab0cdeb9f3b
audino <2676737+audinowho@users.noreply.github.com>
2022-10-02 22:01:34 -0800

    stylistic adjustment

--- RogueEssence/Lua/ScriptUI.cs
+++ RogueEssence/Lua/ScriptUI.cs
@@ -484,9 +484,9 @@ namespace RogueEssence.Script
             List<InvItem> items = new List<InvItem>();
             int item_count = DataManager.Instance.Save.ActiveTeam.GetInvCount();
     
-            for (int i = 0; i < item_count; i++) {
+            for (int ii = 0; ii < item_count; ii++) {
                 // Get a list of inventory items.
-                InvItem item = DataManager.Instance.Save.ActiveTeam.GetInv(i);
+                InvItem item = DataManager.Instance.Save.ActiveTeam.GetInv(ii);
                 items.Add(item);
             };
 
@@ -494,8 +494,8 @@ namespace RogueEssence.Script
             DataManager.Instance.Save.ActiveTeam.StoreItems(items);
 
             // Remove the items back to front to prevent removing them in the wrong order.
-            for (int i = items.Count - 1; i >= 0; i--) {
-                DataManager.Instance.Save.ActiveTeam.RemoveFromInv(i);
+            for (int ii = items.Count - 1; ii >= 0; ii--) {
+                DataManager.Instance.Save.ActiveTeam.RemoveFromInv(ii);
             }
         }
 
