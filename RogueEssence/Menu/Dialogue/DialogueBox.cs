bd3372b1def8b8cdc4cfdab943f6ca5632bf2328
audino <2676737+audinowho@users.noreply.github.com>
2023-01-18 20:48:43 -0800

    smooth transition scrolling with autocomplete dialoguebox

--- RogueEssence/Menu/Dialogue/DialogueBox.cs
+++ RogueEssence/Menu/Dialogue/DialogueBox.cs
@@ -37,7 +37,11 @@ namespace RogueEssence.Menu
         private bool scrolling;
         private bool centerH;
         private bool centerV;
+
+        private int nextTextIndex;
+
         protected DialogueText CurrentText { get { return Texts[curTextIndex]; } }
+        protected DialogueText NextText { get { return nextTextIndex > -1 ? Texts[nextTextIndex] : null; } }
         public bool Finished { get { return CurrentText.Finished && curTextIndex == Texts.Count-1; } }
         public bool Sound;
 
@@ -138,16 +142,22 @@ namespace RogueEssence.Menu
                     || input.JustPressed(FrameInput.InputType.LeftMouse))
                 {
                     scrolling = true;
+                    nextTextIndex = curTextIndex + 1;
+                    NextText.Rect.Start = CurrentText.Rect.Start + new Loc(0, TEXT_HEIGHT * MAX_LINES);
                 }
 
                 if (scrolling)
+                {
                     CurrentText.Rect.Start -= new Loc(0, SCROLL_SPEED);
+                    NextText.Rect.Start -= new Loc(0, SCROLL_SPEED);
+                }
                 int scrollFrames = TEXT_HEIGHT * MAX_LINES / SCROLL_SPEED;
                 if (CurrentScrollTime >= FrameTick.FromFrames(scrollFrames))
                 {
+                    nextTextIndex = -1;
+                    scrolling = false;
                     curTextIndex++;
                     CurrentScrollTime = new FrameTick();
-                    scrolling = false;
                 }
             }
             else
@@ -185,6 +195,8 @@ namespace RogueEssence.Menu
         public override IEnumerable<IMenuElement> GetElements()
         {
             yield return CurrentText;
+            if (nextTextIndex > -1)
+                yield return NextText;
         }
 
         protected TextPause getCurrentTextPause()
@@ -238,7 +250,8 @@ namespace RogueEssence.Menu
 
         public void FinishText()
         {
-            CurrentText.FinishText();
+            foreach(DialogueText text in Texts)
+                text.FinishText();
         }
 
         private void updateMessage()
@@ -247,6 +260,7 @@ namespace RogueEssence.Menu
             //and colors, which will get parsed by the text renderer
             Texts.Clear();
             curTextIndex = 0;
+            nextTextIndex = -1;
             Pauses.Clear();
             ScriptCalls.Clear();
 
