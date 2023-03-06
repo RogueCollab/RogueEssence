7294d61636408413f7d44a7d41d671860a7608ba
audino <2676737+audinowho@users.noreply.github.com>
2023-03-06 15:22:27 -0800

    Team Editor stub

---
+++ RogueEssence.Editor.Avalonia/ViewModels/UserControls/TeamBoxViewModel.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using RogueElements;
+using RogueEssence.Dungeon;
+using ReactiveUI;
+using RogueEssence.Content;
+using Avalonia.Controls;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using RogueEssence.Dev.Views;
+
+namespace RogueEssence.Dev.ViewModels
+{
+
+    public class TeamMemberNodeViewModel : ViewModelBase
+    {
+
+        private string name;
+        public string Name
+        {
+            get => name;
+            set => this.SetIfChanged(ref name, value);
+        }
+
+        public TeamMemberNodeViewModel(string name)
+        {
+            this.name = name;
+        }
+
+    }
+
+    public class TeamNodeViewModel : ViewModelBase
+    {
+
+        private string name;
+        public string Name
+        {
+            get => name;
+            set => this.SetIfChanged(ref name, value);
+        }
+
+        public ObservableCollection<TeamMemberNodeViewModel> Nodes { get; }
+
+        public TeamNodeViewModel(string name)
+        {
+            this.name = name;
+            Nodes = new ObservableCollection<TeamMemberNodeViewModel>();
+        }
+
+    }
+
+    public class TeamBoxViewModel : ViewModelBase
+    {
+        protected UndoStack edits { get; }
+
+        public TeamBoxViewModel(UndoStack stack)
+        {
+            this.edits = stack;
+            Teams = new WrappedObservableCollection<Team>();
+        }
+
+        public WrappedObservableCollection<Team> Teams { get; }
+
+        public event Action SelectedTeamChanged;
+
+
+        private Team chosenTeam;
+        public Team ChosenTeam
+        {
+            get => chosenTeam;
+            set
+            {
+                this.SetIfChanged(ref chosenTeam, value);
+                SelectedTeamChanged?.Invoke();
+            }
+        }
+
+        public async Task EditTeam()
+        {
+            TeamWindow window = new TeamWindow();
+            TeamViewModel vm = new TeamViewModel(chosenTeam);
+            window.DataContext = vm;
+
+            DevForm form = (DevForm)DiagManager.Instance.DevEditor;
+
+            bool result = await window.ShowDialog<bool>(form.MapEditForm);
+
+            //lock (GameBase.lockObj)
+            //{
+            //    if (result)
+            //    {
+            //        AnimLayer newLayer = new AnimLayer(vm.Name);
+            //        AnimLayer oldLayer = Layers[ChosenLayer];
+            //        newLayer.Layer = vm.Front ? DrawLayer.Top : DrawLayer.Bottom;
+            //        newLayer.Visible = oldLayer.Visible;
+            //        newLayer.Anims = oldLayer.Anims;
+
+            //        edits.Apply(new GroundDecorationStateUndo(ChosenLayer));
+
+            //        Layers[ChosenLayer] = newLayer;
+            //    }
+            //}
+        }
+
+        //public void AddTeam()
+        //{
+        //    lock (GameBase.lockObj)
+        //    {
+        //        int curLayer = chosenLayer;
+        //        T layer = GetNewLayer();
+
+        //        edits.Apply(new AddLayerUndo<T>(Layers, chosenLayer + 1, layer, false));
+        //        ChosenLayer = curLayer + 1;
+        //    }
+        //}
+
+        //public void DeleteTeam()
+        //{
+        //    lock (GameBase.lockObj)
+        //    {
+        //        if (Layers.Count > 1)
+        //        {
+        //            int curLayer = chosenLayer;
+
+        //            edits.Apply(new AddLayerUndo<T>(Layers, chosenLayer, Layers[chosenLayer], true));
+
+        //            ChosenLayer = Math.Min(curLayer, Layers.Count - 1);
+        //        }
+        //    }
+        //}
+
+        //public void MoveTeamUp()
+        //{
+        //    lock (GameBase.lockObj)
+        //    {
+        //        if (chosenLayer > 0)
+        //        {
+        //            int insertLayer = chosenLayer - 1;
+        //            edits.Apply(new MoveLayerUndo<T>(Layers, insertLayer));
+        //            ChosenLayer = insertLayer;
+        //        }
+        //    }
+        //}
+
+        //public void MoveTeamDown()
+        //{
+        //    lock (GameBase.lockObj)
+        //    {
+        //        if (chosenLayer < Layers.Count - 1)
+        //        {
+        //            int insertLayer = chosenLayer + 1;
+        //            edits.Apply(new MoveLayerUndo<T>(Layers, chosenLayer));
+        //            ChosenLayer = insertLayer;
+        //        }
+        //    }
+        //}
+
+        public void LoadTeams()
+        {
+            if (Design.IsDesignMode)
+                return;
+
+            Teams.LoadModels(ZoneManager.Instance.CurrentMap.MapTeams);
+        }
+    }
+
+}
---
+++ RogueEssence.Editor.Avalonia/Views/UserControls/TeamBox.axaml.cs
@@ -0,0 +1,42 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using RogueEssence.Dev.ViewModels;
+using RogueEssence.Dungeon;
+using Avalonia.Input;
+
+namespace RogueEssence.Dev.Views
+{
+    public class TeamBox : UserControl
+    {
+        public TeamBox()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        bool doubleclick;
+        public void doubleClickStart(object sender, RoutedEventArgs e)
+        {
+            doubleclick = true;
+        }
+
+        public async void lbxItems_DoubleClick(object sender, PointerReleasedEventArgs e)
+        {
+            if (!doubleclick)
+                return;
+            doubleclick = false;
+
+            TeamBoxViewModel viewModel = (TeamBoxViewModel)DataContext;
+            if (viewModel == null)
+                return;
+
+            await viewModel.EditTeam();
+        }
+    }
+}
