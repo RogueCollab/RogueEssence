100431b715dc4e3f99b9f9973fe6ac984cbd9e7a
audino <2676737+audinowho@users.noreply.github.com>
2023-08-06 18:03:37 -0800

    Multiplier editor

---
+++ RogueEssence/Dev/CustomAttributes/FractionLimitAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RogueEssence.Dev
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class FractionLimitAttribute : PassableAttribute
+    {
+        public readonly int NumSign;
+        public readonly int DenSign;
+        public FractionLimitAttribute(int flags, int numSign, int denSign) : base(flags)
+        {
+            NumSign = numSign;
+            DenSign = denSign;
+        }
+
+        public void GetMinVals(out int numMin, out int denMin)
+        {
+            numMin = NumSign;
+            if (numMin < 0)
+                numMin = Int32.MinValue;
+            denMin = DenSign;
+            if (denMin < 0)
+                denMin = Int32.MinValue;
+
+        }
+    }
+}
--- RogueEssence/Dungeon/GameEffects/Multiplier.cs
+++ RogueEssence/Dungeon/GameEffects/Multiplier.cs
@@ -50,6 +50,11 @@
             else
                 return baseAmt * Numerator / Denominator;
         }
+
+        public override string ToString()
+        {
+            return string.Format("{0}/{1}", Numerator, Denominator);
+        }
     }
 
 }
