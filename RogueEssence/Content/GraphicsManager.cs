79718b10aa297fd1b1712858fe7f7dc0a901bed1
audino <2676737+audinowho@users.noreply.github.com>
2023-10-24 13:37:17 -0800

    separate zh conversion

--- RogueEssence/Content/GraphicsManager.cs
+++ RogueEssence/Content/GraphicsManager.cs
@@ -583,7 +583,7 @@ namespace RogueEssence.Content
             if ((conversionFlags & AssetType.Font) != AssetType.None)
             {
                 Dev.ImportHelper.ImportFonts(PathMod.DEV_PATH + "Font/", BASE_PATH + "{0}.font", "system");
-                Dev.ImportHelper.ImportFonts(PathMod.DEV_PATH + "Font/", PathMod.HardMod(FONT_PATTERN), "green", "blue", "yellow", "text", "banner");
+                Dev.ImportHelper.ImportFonts(PathMod.DEV_PATH + "Font/", PathMod.HardMod(FONT_PATTERN), "green", "blue", "yellow", "text", "text.zh-hans", "banner");
             }
             if ((conversionFlags & AssetType.Chara) != AssetType.None)
             {
