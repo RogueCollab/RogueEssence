5c164b2a58877af89e3bf21afad58aa4445f4865
MistressNebula <delbovemarie@gmail.com>
2024-05-19 21:22:38 +0200

    Add ways to get a choice's index by its label
    The function allows for multiple labels to be requested at the same time, too.

--- RogueEssence/Menu/MultiPageMenu.cs
+++ RogueEssence/Menu/MultiPageMenu.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using RogueElements;
 using RogueEssence.Content;
 
@@ -151,6 +152,45 @@ namespace RogueEssence.Menu
         public void ImportTotalChoices(IChoosable[] choices)
         {
             TotalChoices = SortIntoPages(choices, SpacesPerPage);
+        }
+
+        public override int GetChoiceIndexByLabel(string label)
+        {
+            return GetChoiceIndexesByLabel(label)[label];
+        }
+        public override Dictionary<string, int> GetChoiceIndexesByLabel(params string[] labels)
+        {
+            Dictionary<string, int> poss = new();
+            List<string> labelList = labels.ToList();
+            foreach (string label in labels)
+                poss.Add(label, -1);
+
+            for (int ii = 0; ii < TotalChoices.Length; ii++)
+            {
+                bool found = false;
+                IChoosable[] page = TotalChoices[ii];
+                for (int jj = 0; jj < TotalChoices.Length; jj++)
+                {
+                    IChoosable choice = page[jj];
+                    if (choice.HasLabel())
+                    {
+                        for (int kk = 0; kk < labelList.Count; kk++)
+                        {
+                            string label = labelList[kk];
+                            if(choice.Label == label)
+                            {
+                                found = true;
+                                poss[label] = ii * SpacesPerPage + jj;
+                                labelList.RemoveAt(kk);
+                                break;
+                            }
+                        }
+                    }
+                    if (found) break;
+                }
+                if (found && labelList.Count == 0) break;
+            }
+            return poss;
         }
     }
 }
