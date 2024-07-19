d06f658a5858796e9ef2bb3c67be60d1c279a10a
MistressNebula <delbovemarie@gmail.com>
2024-07-19 02:42:37 +0200

    Remove unnecessary usings

--- RogueEssence/Menu/MenuElements/MenuChoice.cs
+++ RogueEssence/Menu/MenuElements/MenuChoice.cs
@@ -4,7 +4,6 @@ using RogueElements;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using RogueEssence.Content;
-using System.Linq;
 
 namespace RogueEssence.Menu
 {
--- RogueEssence/Menu/MenuElements/MenuStatBar.cs
+++ RogueEssence/Menu/MenuElements/MenuStatBar.cs
@@ -2,7 +2,6 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using RogueEssence.Content;
-using System.Reflection.Emit;
 
 namespace RogueEssence.Menu
 {
