23f832bc740ecaaa74d28c79927468f4bcab9cc1
audino <2676737+audinowho@users.noreply.github.com>
2024-07-31 17:18:55 -0800

    adopt Elements for HotKeyMenu and MemberStatsMenu

--- RogueEssence/Menu/Notices/HotkeyMenu.cs
+++ RogueEssence/Menu/Notices/HotkeyMenu.cs
@@ -21,8 +21,11 @@ namespace RogueEssence.Menu
 
             Bounds = Rect.FromPoints(center - new Loc(56 + GraphicsManager.MenuBG.TileWidth, 0), center + new Loc(56 + GraphicsManager.MenuBG.TileWidth, LINE_HEIGHT * 2 + GraphicsManager.MenuBG.TileHeight * 2));
             skillText = new MenuText("", Bounds.Center + new Loc(-50, GraphicsManager.MenuBG.TileHeight), DirH.Left);
+            Elements.Add(skillText);
             skillElement = new MenuText("", Bounds.Center + new Loc(-48, GraphicsManager.MenuBG.TileHeight + LINE_HEIGHT), DirH.Left);
+            Elements.Add(skillElement);
             skillCharges = new MenuText("", Bounds.Center + new Loc(48, GraphicsManager.MenuBG.TileHeight + LINE_HEIGHT), DirH.Right);
+            Elements.Add(skillCharges);
         }
 
         public void SetArrangement(bool diamond)
@@ -87,12 +90,6 @@ namespace RogueEssence.Menu
                 skillText.Color = preview ? Color.Yellow : skillCharges.Color;
         }
 
-        protected override IEnumerable<IMenuElement> GetDrawElements()
-        {
-            yield return skillText;
-            yield return skillCharges;
-            yield return skillElement;
-        }
     }
 
 
@@ -103,11 +100,7 @@ namespace RogueEssence.Menu
         public PreviewSkillMenu()
         {
             menuText = new MenuText("", Loc.Zero, DirH.Right);
-        }
-
-        protected override IEnumerable<IMenuElement> GetDrawElements()
-        {
-            yield return menuText;
+            Elements.Add(menuText);
         }
 
         public void UpdateControls()
--- RogueEssence/Menu/Team/MemberStatsMenu.cs
+++ RogueEssence/Menu/Team/MemberStatsMenu.cs
@@ -79,14 +79,19 @@ namespace RogueEssence.Menu
             int totalPages = totalLearnsetPages + totalOtherMemberPages;
             
             Title = new MenuText(Text.FormatKey("MENU_STATS_TITLE"), new Loc(GraphicsManager.MenuBG.TileWidth + 8, GraphicsManager.MenuBG.TileHeight));
+            Elements.Add(Title);
             PageText = new MenuText($"(2/{totalPages})", new Loc(Bounds.Width - GraphicsManager.MenuBG.TileWidth, GraphicsManager.MenuBG.TileHeight), DirH.Right);
+            Elements.Add(PageText);
             Div = new MenuDivider(new Loc(GraphicsManager.MenuBG.TileWidth, GraphicsManager.MenuBG.TileHeight + LINE_HEIGHT), Bounds.Width - GraphicsManager.MenuBG.TileWidth * 2);
-
+            Elements.Add(Div);
 
             Portrait = new SpeakerPortrait(player.BaseForm, new EmoteStyle(0),
                 new Loc(GraphicsManager.MenuBG.TileWidth * 2, GraphicsManager.MenuBG.TileHeight + TitledStripMenu.TITLE_OFFSET), false);
+            Elements.Add(Portrait);
+
             string speciesText = player.GetDisplayName(true) + " / " + CharData.GetFullFormName(player.BaseForm);
             Name = new MenuText(speciesText, new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 48, GraphicsManager.MenuBG.TileHeight + TitledStripMenu.TITLE_OFFSET));
+            Elements.Add(Name);
 
             ElementData element1 = DataManager.Instance.GetElement(player.Element1);
             ElementData element2 = DataManager.Instance.GetElement(player.Element2);
@@ -98,10 +103,13 @@ namespace RogueEssence.Menu
             bool origElements = (player.Element1 == monsterForm.Element1);
             origElements &= (player.Element2 == monsterForm.Element2);
             CharElements = new MenuText(Text.FormatKey("MENU_TEAM_ELEMENT", typeString), new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 48, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 1 + TitledStripMenu.TITLE_OFFSET), origElements ? Color.White : Color.Yellow);
+            Elements.Add(CharElements);
 
             LevelLabel = new MenuText(Text.FormatKey("MENU_TEAM_LEVEL_SHORT"), new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 48, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 2 + TitledStripMenu.TITLE_OFFSET));
+            Elements.Add(LevelLabel);
             Level = new MenuText(player.Level.ToString(),  new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 48 + GraphicsManager.TextFont.SubstringWidth(Text.FormatKey("MENU_TEAM_LEVEL_SHORT")), GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 2 + TitledStripMenu.TITLE_OFFSET), DirH.Left);
-            
+            Elements.Add(Level);
+
             int expToNext = 0;
             if (player.Level < DataManager.Instance.Start.MaxLevel)
             {
@@ -111,50 +119,83 @@ namespace RogueEssence.Menu
             }
             EXP = new MenuText(Text.FormatKey("MENU_TEAM_EXP", player.EXP, expToNext),
                 new Loc(GraphicsManager.MenuBG.TileWidth * 2, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 3 + TitledStripMenu.TITLE_OFFSET));
+            Elements.Add(EXP);
 
             MainDiv = new MenuDivider(new Loc(GraphicsManager.MenuBG.TileWidth, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 5), Bounds.Width - GraphicsManager.MenuBG.TileWidth * 2);
+            Elements.Add(MainDiv);
 
             StatsTitle = new MenuText(Text.FormatKey("MENU_TEAM_STATS"), new Loc(GraphicsManager.MenuBG.TileWidth * 2, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 4 + TitledStripMenu.TITLE_OFFSET));
+            Elements.Add(StatsTitle);
             BonusTooltip = new MenuText(Text.FormatKey("MENU_TEAM_BONUS", DiagManager.Instance.GetControlString(FrameInput.InputType.SortItems)), new Loc(Bounds.Width - GraphicsManager.MenuBG.TileWidth * 2, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 4 + TitledStripMenu.TITLE_OFFSET), DirH.Right);
+            Elements.Add(BonusTooltip);
 
             HPLabel = new MenuText(Text.FormatKey("MENU_LABEL", Stat.HP.ToLocal("tiny")), new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 8, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 5 + TitledStripMenu.TITLE_OFFSET));
+            Elements.Add(HPLabel);
             AttackLabel = new MenuText(Text.FormatKey("MENU_LABEL", Stat.Attack.ToLocal("tiny")), new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 8, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 6 + TitledStripMenu.TITLE_OFFSET), player.ProxyAtk > -1 ? Color.Yellow : Color.White);
+            Elements.Add(AttackLabel);
             DefenseLabel = new MenuText(Text.FormatKey("MENU_LABEL", Stat.Defense.ToLocal("tiny")), new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 8, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 7 + TitledStripMenu.TITLE_OFFSET), player.ProxyDef > -1 ? Color.Yellow : Color.White);
+            Elements.Add(DefenseLabel);
             MAtkLabel = new MenuText(Text.FormatKey("MENU_LABEL", Stat.MAtk.ToLocal("tiny")), new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 8, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 8 + TitledStripMenu.TITLE_OFFSET), player.ProxyMAtk > -1 ? Color.Yellow : Color.White);
+            Elements.Add(MAtkLabel);
             MDefLabel = new MenuText(Text.FormatKey("MENU_LABEL", Stat.MDef.ToLocal("tiny")), new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 8, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 9 + TitledStripMenu.TITLE_OFFSET), player.ProxyMDef > -1 ? Color.Yellow : Color.White);
+            Elements.Add(MDefLabel);
             SpeedLabel = new MenuText(Text.FormatKey("MENU_LABEL", Stat.Speed.ToLocal("tiny")), new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 8, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 10 + TitledStripMenu.TITLE_OFFSET), player.ProxySpeed > -1 ? Color.Yellow : Color.White);
+            Elements.Add(SpeedLabel);
 
             HP = new MenuText(player.MaxHP.ToString(), new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 72, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 5 + TitledStripMenu.TITLE_OFFSET), DirH.Right);
+            Elements.Add(HP);
             Attack = new MenuText(player.Atk.ToString(), new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 72, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 6 + TitledStripMenu.TITLE_OFFSET), DirV.Up, DirH.Right, player.ProxyAtk > -1 ? Color.Yellow : Color.White);
+            Elements.Add(Attack);
             Defense = new MenuText(player.Def.ToString(), new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 72, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 7 + TitledStripMenu.TITLE_OFFSET), DirV.Up, DirH.Right, player.ProxyDef > -1 ? Color.Yellow : Color.White);
+            Elements.Add(Defense);
             MAtk = new MenuText(player.MAtk.ToString(), new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 72, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 8 + TitledStripMenu.TITLE_OFFSET), DirV.Up, DirH.Right, player.ProxyMAtk > -1 ? Color.Yellow : Color.White);
+            Elements.Add(MAtk);
             MDef = new MenuText(player.MDef.ToString(), new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 72, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 9 + TitledStripMenu.TITLE_OFFSET), DirV.Up, DirH.Right, player.ProxyMDef > -1 ? Color.Yellow : Color.White);
+            Elements.Add(MDef);
             Speed = new MenuText(player.Speed.ToString(), new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 72, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 10 + TitledStripMenu.TITLE_OFFSET), DirV.Up, DirH.Right, player.ProxySpeed > -1 ? Color.Yellow : Color.White);
+            Elements.Add(Speed);
 
             int hpLength = calcLength(Stat.HP, monsterForm, player.MaxHP, player.Level);
             HPBar = new MenuStatBar(new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 76, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 5 + TitledStripMenu.TITLE_OFFSET), hpLength, calcColor(hpLength));
+            Elements.Add(HPBar);
             int atkLength = calcLength(Stat.Attack, monsterForm, player.Atk, player.Level);
             AttackBar = new MenuStatBar(new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 76, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 6 + TitledStripMenu.TITLE_OFFSET), atkLength, calcColor(atkLength));
+            Elements.Add(AttackBar);
             int defLength = calcLength(Stat.Defense, monsterForm, player.Def, player.Level);
             DefenseBar = new MenuStatBar(new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 76, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 7 + TitledStripMenu.TITLE_OFFSET), defLength, calcColor(defLength));
+            Elements.Add(DefenseBar);
             int mAtkLength = calcLength(Stat.MAtk, monsterForm, player.MAtk, player.Level);
             MAtkBar = new MenuStatBar(new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 76, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 8 + TitledStripMenu.TITLE_OFFSET), mAtkLength, calcColor(mAtkLength));
+            Elements.Add(MAtkBar);
             int mDefLength = calcLength(Stat.MDef, monsterForm, player.MDef, player.Level);
             MDefBar = new MenuStatBar(new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 76, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 9 + TitledStripMenu.TITLE_OFFSET), mDefLength, calcColor(mDefLength));
+            Elements.Add(MDefBar);
             int speedLength = calcLength(Stat.Speed, monsterForm, player.Speed, player.Level);
             SpeedBar = new MenuStatBar(new Loc(GraphicsManager.MenuBG.TileWidth * 2 + 76, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 10 + TitledStripMenu.TITLE_OFFSET), speedLength, calcColor(speedLength));
+            Elements.Add(SpeedBar);
+
 
             HPBonus = new MenuText("", new Loc(Bounds.Width - GraphicsManager.MenuBG.TileWidth * 2 - 8, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 5 + TitledStripMenu.TITLE_OFFSET), DirH.Right);
+            Elements.Add(HPBonus);
             AttackBonus = new MenuText("", new Loc(Bounds.Width - GraphicsManager.MenuBG.TileWidth * 2 - 8, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 6 + TitledStripMenu.TITLE_OFFSET),  DirH.Right);
+            Elements.Add(AttackBonus);
             DefenseBonus = new MenuText("", new Loc(Bounds.Width - GraphicsManager.MenuBG.TileWidth * 2 - 8, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 7 + TitledStripMenu.TITLE_OFFSET), DirH.Right);
+            Elements.Add(DefenseBonus);
             MAtkBonus = new MenuText("", new Loc(Bounds.Width - GraphicsManager.MenuBG.TileWidth * 2 - 8, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 8 + TitledStripMenu.TITLE_OFFSET), DirH.Right);
+            Elements.Add(MAtkBonus);
             MDefBonus = new MenuText("", new Loc(Bounds.Width - GraphicsManager.MenuBG.TileWidth * 2 - 8, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 9 + TitledStripMenu.TITLE_OFFSET), DirH.Right);
+            Elements.Add(MDefBonus);
             SpeedBonus = new MenuText("", new Loc(Bounds.Width - GraphicsManager.MenuBG.TileWidth * 2 - 8, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 10 + TitledStripMenu.TITLE_OFFSET), DirH.Right);
+            Elements.Add(SpeedBonus);
 
             ItemDiv = new MenuDivider(new Loc(GraphicsManager.MenuBG.TileWidth, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 12), Bounds.Width - GraphicsManager.MenuBG.TileWidth * 2);
+            Elements.Add(ItemDiv);
 
             Item = new MenuText(!String.IsNullOrEmpty(player.EquippedItem.ID) ? Text.FormatKey("MENU_HELD_ITEM", player.EquippedItem.GetDisplayName()) : Text.FormatKey("MENU_HELD_NO_ITEM"), new Loc(GraphicsManager.MenuBG.TileWidth * 2, GraphicsManager.MenuBG.TileHeight + VERT_SPACE * 11 + TitledStripMenu.TITLE_OFFSET));
-            if (bonusView) SwitchView();
+            Elements.Add(Item);
+
+            if (bonusView)
+                SwitchView();
         }
 
         private int calcBoostLength(Stat stat, Character player)
@@ -194,56 +235,6 @@ namespace RogueEssence.Menu
                 return new Color(88, 248, 88);
             else
                 return new Color(88, 192, 248);
-        }
-
-        protected override IEnumerable<IMenuElement> GetDrawElements()
-        {
-            yield return Title;
-            yield return PageText;
-            yield return Div;
-
-            yield return Portrait;
-            yield return Name;
-
-            yield return LevelLabel;
-            yield return Level;
-            yield return CharElements;
-            yield return EXP;
-
-            yield return MainDiv;
-
-            yield return StatsTitle;
-            yield return BonusTooltip;
-            yield return HPLabel;
-            yield return SpeedLabel;
-            yield return AttackLabel;
-            yield return DefenseLabel;
-            yield return MAtkLabel;
-            yield return MDefLabel;
-
-            yield return HP;
-            yield return Speed;
-            yield return Attack;
-            yield return Defense;
-            yield return MAtk;
-            yield return MDef;
-
-            yield return HPBar;
-            yield return SpeedBar;
-            yield return AttackBar;
-            yield return DefenseBar;
-            yield return MAtkBar;
-            yield return MDefBar;
-
-            yield return HPBonus;
-            yield return SpeedBonus;
-            yield return AttackBonus;
-            yield return DefenseBonus;
-            yield return MAtkBonus;
-            yield return MDefBonus;
-
-            yield return ItemDiv;
-            yield return Item;
         }
 
         public override void Update(InputManager input)
