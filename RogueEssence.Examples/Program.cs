dc241dce6ef7b2d395ecad3614735b0f259e1919
audino <2676737+audinowho@users.noreply.github.com>
2024-09-01 15:23:56 -0800

    fix argument negation

--- RogueEssence.Examples/Program.cs
+++ RogueEssence.Examples/Program.cs
@@ -95,7 +95,7 @@ namespace RogueEssence.Examples
                     {
                         string appName = Path.GetFileNameWithoutExtension(args[0]);
                         PathMod.APP_PATH = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), appName) + "/";
-                        if (args.Length > ii + 1 && args[ii + 1].StartsWith("-"))
+                        if (args.Length > ii + 1 && !args[ii + 1].StartsWith("-"))
                         {
                             PathMod.APP_PATH = Path.GetFullPath(args[ii + 1]);
                             ii++;
