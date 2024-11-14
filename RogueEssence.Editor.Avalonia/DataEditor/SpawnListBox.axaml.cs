52ff1d78a577cd3260293e81d0d752043335ec63
Kace <lakacey03@gmail.com>
2024-11-14 00:28:46 -0500

    shift click modifier for SpawnListBox

--- RogueEssence.Editor.Avalonia/DataEditor/SpawnListBox.axaml.cs
+++ RogueEssence.Editor.Avalonia/DataEditor/SpawnListBox.axaml.cs
@@ -8,6 +8,7 @@ using System.Collections;
 using System.Collections.ObjectModel;
 using System.Reactive.Subjects;
 using Avalonia.Input;
+using RogueEssence.Dev.ViewModels;
 
 namespace RogueEssence.Dev.Views
 {
@@ -17,6 +18,8 @@ namespace RogueEssence.Dev.Views
         public SpawnListBox()
         {
             this.InitializeComponent();
+            Button button = this.FindControl<Button>("SpawnListBoxAddButton");
+            button.AddHandler(PointerReleasedEvent, SpawnListBoxAddButton_OnPointerReleased, RoutingStrategies.Tunnel);
         }
 
         private void InitializeComponent()
@@ -46,6 +49,14 @@ namespace RogueEssence.Dev.Views
         {
             DataGrid lbx = this.FindControl<DataGrid>("gridItems");
             lbx.ContextMenu = menu;
+        }
+
+        private void SpawnListBoxAddButton_OnPointerReleased(object sender, PointerReleasedEventArgs e)
+        {
+            KeyModifiers modifiers = e.KeyModifiers;
+            bool advancedEdit = modifiers.HasFlag(KeyModifiers.Shift);
+            SpawnListBoxViewModel vm = (SpawnListBoxViewModel) DataContext;
+            vm.btnAdd_Click(advancedEdit);
         }
     }
 }
