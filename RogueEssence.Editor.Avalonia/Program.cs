e7b60f284800cfed7950a6287f87d9997b6d0648
Kace <41309226+DoubleTrio@users.noreply.github.com>
2025-11-15 22:24:22 -0500

    minor fixes

--- RogueEssence.Editor.Avalonia/Program.cs
+++ RogueEssence.Editor.Avalonia/Program.cs
@@ -10,7 +10,6 @@ namespace RogueEssence.Dev
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
-        [STAThread]
         public static void Main(string[] args)
         {
             Native.OS.SetupDataDir();
