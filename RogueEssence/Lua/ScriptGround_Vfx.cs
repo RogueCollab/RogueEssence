8c700e1dffc214a3c3355d8d5f7c3d8dbb6f7afb
audino <2676737+audinowho@users.noreply.github.com>
2026-08-05 16:21:01 -0800

    rename 1

--- RogueEssence/Lua/ScriptGround_Vfx.cs
+++ RogueEssence/Lua/ScriptGround_Vfx.cs
@@ -17,6 +17,7 @@ namespace RogueEssence.Script
     public partial class ScriptGround : ILuaEngineComponent
     {
 
+
         //===================================
         //  VFX
         //===================================
